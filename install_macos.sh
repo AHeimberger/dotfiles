627b46f4dcb46fe6c62b9f4bec1dd8fd927a8a3c
aheimberger <AndreasHeimberger@gmx.at>
2024-02-13 21:02:05 +0100

    chore: add missing vim plugin

--- install_macos.sh
+++ install_macos.sh
@@ -35,5 +35,19 @@ for FILE in ${FILES[@]}; do
     ln -s "${DIR_CURRENT}/${FILE}" "${HOME}/.${FILE}"
 done
 
-echo "- installing themes"
+echo "- installing helix theme"
 git clone "https://github.com/CptPotato/helix-themes.git" "${HOME}/.config/helix/themes"
+
+echo "- installing vim plugins"
+rm -rf ${HOME}/.vim
+mkdir -p ${HOME}/.vim/autoload ${HOME}/.vim/bundle ${HOME}/.vim/colors
+curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O pathogen.vim
+mv pathogen.vim ${HOME}/.vim/autoload/pathogen.vim
+git clone https://github.com/scrooloose/nerdtree.git ${HOME}/.vim/bundle/nerdtree
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ${HOME}/.vim/bundle/nerdtree-git-plugin
+git clone https://github.com/vim-syntastic/syntastic.git ${HOME}/.vim/bundle/syntastic
+git clone https://github.com/airblade/vim-gitgutter.git ${HOME}/.vim/bundle/vim-gitgutter
+git clone https://github.com/tpope/vim-fugitive.git ${HOME}/.vim/bundle/vim-fugitive
+git clone git@github.com:zsoltf/vim-maui.git ${HOME}/vim-maui
+mv ${HOME}/vim-maui/colors/maui.vim ${HOME}/.vim/colors/
+rm -rf ${HOME}/vim-maui
